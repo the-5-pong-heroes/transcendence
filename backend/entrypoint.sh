73ca2d88cd006782f05c5624c73da56fd9546e66
llalba <llalba@student.42.fr>
2023-02-10 19:39:35 +0100

    🐳 Docker config for backend & DB looks OK

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+# evaluates the env variable expected by Prisma
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+
+echo "💭 Syncing the migration history..."
+npx prisma migrate deploy
+echo "✅ Migration files successfully run"
+
+echo "🚀 Launching NestJS..."
+npm run start:prod

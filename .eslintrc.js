e1c64f469e891590a5e5f7f41361d8af4254ef38
llalba <llalba@student.42.fr>
2023-02-15 14:22:17 +0100

    🤯 Comments in frontend/eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,23 @@
 // https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
 
 module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
     "prettier",
+    // TODO eslint-plugin-node
   ],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": 2, // Means error
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
@@ -15,16 +27,5 @@ module.exports = {
   },
   plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
   ignorePatterns: [".eslintrc.js"],
-  rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "prettier/prettier": 2, // Means error
-  },
 };

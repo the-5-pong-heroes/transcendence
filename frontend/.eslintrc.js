32c50e23f668626a7c5e02a842d48a611f513aef
EugenieF <eug.francon@gmail.com>
2023-02-17 11:24:04 +0100

    Problem compilation with new vite.config.ts

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,7 +4,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2022: true,
   },
   extends: [
     "eslint:recommended",
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //   server: {
+  //     watch: {
+  //       // file system watcher options
+  //       usePolling: true,
+  //     },
+  //     host: true, // listens on all addresses and not only localhost
+  //     strictPort: true, // exits when the port is not available
+  //     port: parseInt(process.env.FRONTEND_PORT),
+  //   },
+});

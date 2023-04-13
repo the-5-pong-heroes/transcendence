76ca6bf0346bef62616e9cf972ab8eb80012ac18
Cavallucci <laura.cllucci@gmail.com>
2023-04-13 16:59:30 +0200

    pb email prisma

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,10 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class AuthDto {
-  // @IsEmail()
-  // @IsNotEmpty()
-  // email: string
-  // @IsString()
-  // @IsNotEmpty()
-  // password: string
+    @IsEmail()
+    @IsNotEmpty()
+    email: string = 'test';
+
+    @IsString()
+    @IsNotEmpty()
+    password: string = 'test';
 }

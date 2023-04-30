c45b43cbaf397ee18d863828883385c6f08247eb
llalba <llalba@student.42.fr>
2023-04-30 15:15:26 +0200

    🐳 Docker config updated for the shared directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import * as path from "path";
 
 // https://vitejs.dev/config/

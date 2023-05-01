a99928fcc7f7f9cf357d423596f24e80823f6182
Marion & Louis <llalba@student.42.fr>
2023-05-01 15:36:13 +0200

    🌱 Added friendships to seed.ts

--- backend/src/prisma/seed.ts
+++ backend/src/prisma/seed.ts
@@ -12,6 +12,12 @@ async function createUsers() {
         status: "ONLINE",
       },
       {
+        id: "011500e7-4c91-4f97-b41f-d2678a8e773e",
+        name: "Robtoine Collant",
+        avatar: "https://example.com/avatar2.jpg",
+        status: "IN_GAME",
+      },
+      {
         id: "4e0e94c6-f526-4346-b2f5-b51c7ea9ba5c",
         name: "Jane Smith",
         avatar: "https://example.com/avatar2.jpg",
@@ -52,9 +58,25 @@ async function createGames() {
   });
 }
 
+async function createFriendships() {
+  await prisma.friendship.createMany({
+    data: [
+      {
+        userId: "bb7d87d5-dba5-4461-b462-e577a210e827",
+        addedById: "011500e7-4c91-4f97-b41f-d2678a8e773e",
+      },
+      {
+        userId: "011500e7-4c91-4f97-b41f-d2678a8e773e",
+        addedById: "4e0e94c6-f526-4346-b2f5-b51c7ea9ba5c",
+      },
+    ],
+  });
+}
+
 async function main() {
   createUsers().then(async () => {
     await createGames();
+    await createFriendships();
   });
 }
 

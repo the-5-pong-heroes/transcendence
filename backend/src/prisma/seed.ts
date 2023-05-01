458da4b16678d92c648790cd368b369cf3e6d075
Marion & Louis <llalba@student.42.fr>
2023-05-01 11:27:03 +0200

    🌱 Added games to seed.ts

--- backend/src/prisma/seed.ts
+++ backend/src/prisma/seed.ts
@@ -2,15 +2,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const users = await prisma.user.createMany({
+async function createUsers() {
+  await prisma.user.createMany({
     data: [
       {
+        id: "bb7d87d5-dba5-4461-b462-e577a210e827",
         name: "John Doe",
         avatar: "https://example.com/avatar1.jpg",
         status: "ONLINE",
       },
       {
+        id: "4e0e94c6-f526-4346-b2f5-b51c7ea9ba5c",
         name: "Jane Smith",
         avatar: "https://example.com/avatar2.jpg",
         status: "OFFLINE",
@@ -18,6 +20,44 @@ async function main() {
     ],
   });
 }
+
+async function createGames() {
+  await prisma.game.createMany({
+    data: [
+      {
+        playerOneId: "bb7d87d5-dba5-4461-b462-e577a210e827",
+        playerOneScore: 10,
+        playerTwoId: "4e0e94c6-f526-4346-b2f5-b51c7ea9ba5c",
+        playerTwoScore: 5,
+
+        socketId: "socketId",
+      },
+      {
+        playerOneId: "bb7d87d5-dba5-4461-b462-e577a210e827",
+        playerOneScore: 0,
+        playerTwoId: "4e0e94c6-f526-4346-b2f5-b51c7ea9ba5c",
+        playerTwoScore: 5,
+
+        socketId: "socketId",
+      },
+      {
+        playerOneId: "4e0e94c6-f526-4346-b2f5-b51c7ea9ba5c",
+        playerOneScore: 2,
+        playerTwoId: "bb7d87d5-dba5-4461-b462-e577a210e827",
+        playerTwoScore: 4,
+
+        socketId: "socketId",
+      },
+    ],
+  });
+}
+
+async function main() {
+  createUsers().then(async () => {
+    await createGames();
+  });
+}
+
 main()
   .then(async () => {
     await prisma.$disconnect();

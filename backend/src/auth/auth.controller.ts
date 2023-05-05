3b2c96a426ebca207fd5a6f73ab309c00a2bff32
Cavallucci <laura.cllucci@gmail.com>
2023-05-05 14:34:37 +0200

    tests cookies

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -13,45 +13,30 @@ export class AuthController{
         private Oauth42: Oauth42Service,
         private userService: UserService,)  {}
 
-    // //3333
-    // @Get("google/login")
-    // @UseGuards(GoogleAuthGuard)
-    // handleLogin() {
-    //     return { msg: "Google Authentification "};
-    // }
-
-    // ///auth/google/callback
-    // @Get("/google/callback")
-    // @UseGuards(GoogleAuthGuard)
-    // handleRedirect() {
-    //     return { msg: "OK"};
-    // }
-
-    /***  Create the user in database from the page registration ***/
     @Get("/Oauth42/login")
     async getUserByToken(@Req() req: Request) {
         return await this.authService.getUserByToken(req);
     }
     @Post("Oauth")
     async userOauthCreationInDataBase(@Req() req: Request, @Res() res: Response, @Body() UserDto: UserDto) {
+        console.log("oauthcontroller");
         await this.authService.handleDataBaseCreation(req, res, UserDto);
     }
 
-    /***  After the user said yes to connect to 42 API, we attribute the token and we check if he exists in the database ***/
-    @Get("callback")
+    @Get("/Oauth42/callback")
     async getToken(@Req() req: Request, @Res() res: Response) {
         const codeFromUrl = req.query.code as string;
         const token = await this.Oauth42.accessToken(codeFromUrl);
         const user42infos = await this.Oauth42.access42UserInformation(token.access_token);
 
-//         this.authService.createCookies(res, token);
+         this.authService.createCookies(res, token);
          const userExists = await this.userService.getUserByEmail(user42infos.email);
-//         this.authService.updateCookies(res, token, userExists);
+         this.authService.updateCookies(res, token, userExists);
          this.authService.RedirectConnectingUser(req,res, userExists?.email);
      }
 
-//   @Get("token")
-//   async checkIfTokenValid(@Req() req: Request, @Res() res: Response) {
-//     return this.authService.checkIfTokenValid(req, res);
-//   }
+   @Get("token")
+   async checkIfTokenValid(@Req() req: Request, @Res() res: Response) {
+     return this.authService.checkIfTokenValid(req, res);
+   }
 }

7bde865d8e87298c86bae52ffd14cb05627ee349
Cavallucci <laura.cllucci@gmail.com>
2023-05-06 15:30:26 +0200

    problem getToken with google

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, HttpAdapterHost } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as session from "express-session";
 import * as cookieParser from 'cookie-parser';
+import * as passport from 'passport';
 
 
 async function bootstrap() {
@@ -17,10 +18,19 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
-  app.use(session({ secret: 'secret-key' }));
+  app.enableCors({
+		origin: ['http://localhost:5173', 'http://localhost:3333'],
+		allowedHeaders: ['content-type'],
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+		credentials: true,
+	});
   app.use(cookieParser());
+  app.use(session({ secret: 'secret-key' }));
 
+  app.use(passport.initialize());
+	app.use(passport.session());
   await app.listen(3333); // 3000 might be taken by React
 }
 

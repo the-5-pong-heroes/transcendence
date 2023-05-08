1126b80c6ebfb1a49fdd859ed58ed2c8b8f78e0d
Cavallucci <laura.cllucci@gmail.com>
2023-05-08 15:58:13 +0200

    auth buttons add

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 interface HomeProps {
@@ -7,9 +7,23 @@ interface HomeProps {
 }
 
 export const Home: React.FC<HomeProps> = ({ homeRef }) => {
+  const navigate = useNavigate();
+
+  const handleAuth42 = () => {
+    navigate("/auth/42");
+  };
+
+  const handleAuthGoogle = () => {
+    navigate("/auth/google");
+  };
+
   return (
     <div ref={homeRef} id="Home" className="home">
-      <h1>Home</h1>
-    </div>
+    <h1>Home</h1>
+    <div className="button-group">
+        <button className="button" onClick={handleAuth42}>Connect with 42</button>
+        <button className="button google-button" onClick={handleAuthGoogle}>Connect with Google</button>
+      </div>
+  </div>
   );
 };

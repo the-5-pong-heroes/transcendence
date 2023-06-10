c7b4028bb964ea0f51ec792ca1aab42dadbd4a5e
Eugénie Françon <efrancon@MacBook-Pro-de-Eugenie.local>
2023-06-10 13:04:23 +0200

    [Frontend] Clean game css

--- frontend/src/pages/Game/Game.tsx
+++ frontend/src/pages/Game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 
 import "./Game.css";
@@ -38,72 +38,13 @@ const Pong: React.FC = () => {
   );
 };
 
-// interface FadeOutProps {
-//   children: React.ReactNode;
-// }
-
-// const FadeOutComponent: React.FC<FadeOutProps> = ({ children }) => {
-//   const [visible, setVisible] = useState<boolean>(true);
-
-//   useEffect(() => {
-//     const timeout = setTimeout(() => {
-//       setVisible(false);
-//     }, 2000); // Adjust the delay time as needed
-
-//     return () => clearTimeout(timeout);
-//   }, []);
-
-//   return <div className={`fade-out ${visible ? "visible" : "hidden"}`}>{children}</div>;
-// };
-
-interface useFadeProps {
-  isVisible: boolean;
-  setShow: React.Dispatch<React.SetStateAction<boolean>>;
-  fadeProps: {
-    style: {
-      animation: string;
-    };
-    onAnimationEnd: () => void;
-  };
-}
-
-const useFade = (initial: boolean): useFadeProps => {
-  const [show, setShow] = useState<boolean>(initial);
-  const [isVisible, setVisible] = useState<boolean>(show);
-
-  // Update visibility when show changes
-  useEffect(() => {
-    if (show) {
-      setVisible(true);
-    }
-  }, [show]);
-
-  // When the animation finishes, set visibility to false
-  const onAnimationEnd = (): void => {
-    if (!show) {
-      setVisible(false);
-    }
-  };
-
-  const style = { animation: `${show ? "fadeIn" : "fadeOut"} .3s` };
-
-  // These props go on the fading DOM element
-  const fadeProps = {
-    style,
-    onAnimationEnd,
-  };
-
-  return { isVisible, setShow, fadeProps };
-};
-
 export const Game: React.FC<GameProps> = ({ gameRef }) => {
   const { height, width } = useGameSize();
   const gameStyle: React.CSSProperties = { width, height };
   const { socketReady }: SocketParameters = useSocketContext();
-  const { fadeProps } = useFade(true);
 
   return (
-    <div ref={gameRef} id="Game" className={"game-container"} {...fadeProps}>
+    <div ref={gameRef} id="Game" className={"game-container"}>
       <div className="game" style={gameStyle}>
         <ErrorBoundary
           fallbackRender={fallbackRender}

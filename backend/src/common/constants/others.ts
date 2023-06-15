d5fbd53ffa3a441149c9e9c45d8f6eb1efe35cf4
Marion & Louis <llalba@student.42.fr>
2023-06-15 16:05:55 +0200

    🏪 New const variables

--- backend/src/common/constants/others.ts
+++ backend/src/common/constants/others.ts
@@ -4,3 +4,6 @@ export const LEVEL_THRESHOLD = 4;
 
 export const UPLOAD_URL = "https://api.imgbb.com/1/upload";
 export const IMG_BB_API_KEY = "TODO";
+
+export const MAX_FILE_SIZE = 1024 * 1024 * 4;
+export const VALID_FILE_TYPES = ".(png|jpeg|jpg)";

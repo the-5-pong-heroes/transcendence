b332a93c7f31dd7842565c121ed5b096051eed90
Marion & Louis <llalba@student.42.fr>
2023-06-19 17:13:02 +0200

    ✅ Fixed wrong port in API42_REDIRECT

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -6,4 +6,4 @@ export const API42_URL = "https://api.intra.42.fr/oauth/authorize";
 // The client ID you received from 42 when you registered.
 export const API42_CLIENT_ID = import.meta.env.VITE_API42_CLIENT_ID as string;
 // The URL in your app where users will be sent after authorization.
-export const API42_REDIRECT = "http://localhost:3000/auth/auth42/callback";
+export const API42_REDIRECT = `${BASE_URL}/auth/auth42/callback`;
